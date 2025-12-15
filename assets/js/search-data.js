4408ebcc76f672eb58f34ffc7157253807667874
1saacRankin <56332858+1saacRankin@users.noreply.github.com>
2025-12-15 22:39:36 +0000

    Deploying to gh-pages from @ 1saacRankin/1saacRankin.github.io@2acaad6b9ba39afe493c0b4684cab8b4896264e6 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -37,9 +37,9 @@ ninja.data = [{
           handler: () => {
             window.location.href = "/cv/";
           },
-        },{id: "nav-teaching",
-          title: "teaching",
-          description: "Materials for courses you taught. Replace this text with your description.",
+        },{id: "nav-teaching-experience",
+          title: "Teaching Experience",
+          description: "",
           section: "Navigation",
           handler: () => {
             window.location.href = "/teaching/";
