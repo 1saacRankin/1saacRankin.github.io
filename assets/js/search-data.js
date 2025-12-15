b809a5d6cf531e95410fe63d7a72b76eb5f541da
1saacRankin <56332858+1saacRankin@users.noreply.github.com>
2025-12-15 22:37:55 +0000

    Deploying to gh-pages from @ 1saacRankin/1saacRankin.github.io@f388d04f632a205f972ef37da00f944ccd8ded93 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -37,14 +37,28 @@ ninja.data = [{
           handler: () => {
             window.location.href = "/cv/";
           },
-        },{id: "nav-teaching-experience",
-          title: "Teaching Experience",
+        },{id: "nav-teaching",
+          title: "teaching",
           description: "Materials for courses you taught. Replace this text with your description.",
           section: "Navigation",
           handler: () => {
             window.location.href = "/teaching/";
           },
-        },{id: "post-a-post-with-plotly-js",
+        },{id: "dropdown-bookshelf",
+              title: "bookshelf",
+              description: "",
+              section: "Dropdown",
+              handler: () => {
+                window.location.href = "/books/";
+              },
+            },{id: "dropdown-blog",
+              title: "blog",
+              description: "",
+              section: "Dropdown",
+              handler: () => {
+                window.location.href = "/blog/";
+              },
+            },{id: "post-a-post-with-plotly-js",
         
           title: "a post with plotly.js",
         
