79a2988a1548821f2a28cdcd1c3d3c69126ad168
1saacRankin <56332858+1saacRankin@users.noreply.github.com>
2026-02-05 21:19:15 +0000

    Deploying to gh-pages from @ 1saacRankin/1saacRankin.github.io@b8a87ae0a5c62e4b27dbf6fcba84b8ff58ff2985 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -16,13 +16,6 @@ ninja.data = [{
           handler: () => {
             window.location.href = "/publications/";
           },
-        },{id: "nav-projects",
-          title: "projects",
-          description: "A growing collection of your cool projects.",
-          section: "Navigation",
-          handler: () => {
-            window.location.href = "/projects/";
-          },
         },{id: "nav-curriculum-vitae",
           title: "Curriculum Vitae",
           description: "See the PDF for a full list of work experience, awards, and additional details.",
